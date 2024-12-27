638df0736bfceb3a9c07916600206b89c224be15
Tiarintsoa <rihantiana000@gmail.com>
2024-12-27 11:46:48 +0300

    Initialisation de Spring Boot

---
+++ src/main/java/mg/itu/painorama/PainoramaApplication.java
@@ -0,0 +1,13 @@
+package mg.itu.painorama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainoramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainoramaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/itu/painorama/PainoramaApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.itu.painorama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainoramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
