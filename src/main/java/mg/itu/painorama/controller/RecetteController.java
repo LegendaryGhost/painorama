3813310cadbe487821848e3f1c40ee18b3d0b70d
Tiarintsoa <rihantiana000@gmail.com>
2025-01-14 16:04:14 +0300

    Fix formulaire de recette

--- src/main/java/mg/itu/painorama/controller/RecetteController.java
+++ src/main/java/mg/itu/painorama/controller/RecetteController.java
@@ -3,6 +3,7 @@ package mg.itu.painorama.controller;
 import lombok.AllArgsConstructor;
 import mg.itu.painorama.entity.Recette;
 import mg.itu.painorama.service.IngredientService;
+import mg.itu.painorama.service.ProduitService;
 import mg.itu.painorama.service.RecetteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,12 +19,13 @@ public class RecetteController {
 
     private final IngredientService ingredientService;
     private final RecetteService recetteService;
+    private final ProduitService produitService;
 
     @GetMapping("/ajouter")
     public String ajouterRecette(Model model) {
 	model.addAttribute("recette", new Recette());
 	model.addAttribute("ingredients", ingredientService.findAll());
-	model.addAttribute("produits", ingredientService.findAll());
+	model.addAttribute("produits", produitService.findAll());
 
 	return "recettes/formulaire";
     }
